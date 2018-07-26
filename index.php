481755b4589696e9e7781fbb7d13316b240cfe6c
mac <mac@macs-MacBook-Pro-2.local>
2018-07-26 21:25:48 +0700

    Rubah Element

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>Wani Piro</h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Rubah Data dari local</h1>
+
+  </body>
+</html>
