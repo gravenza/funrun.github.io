f076d5a9d6042f4e539b8f2baa7986f5f2d850ed
mac <mac@macs-MacBook-Pro-2.local>
2018-07-27 21:23:47 +0700

    Create Register Form

---
+++ application/views/registration.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Form registration</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>css/bootstrap.css">
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+          <br>
+          <br>
+          <h1 >FORM REGISTRATION</h1>
+        </div>
+
+        <form class="" style="width:100%" action="index.html" method="post">
+          <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div class="form-group">
+                <label for="">Category :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Field1 :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Field2 :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
