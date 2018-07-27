d5bfd9b867ca6f52cdb16d044e8d6e0bc50757b7
mac <mac@macs-MacBook-Pro-2.local>
2018-07-27 21:32:53 +0700

    Add Button

--- application/views/registration.php
+++ application/views/registration.php
@@ -39,6 +39,13 @@
               </div>
             </div>
 
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <button type="button" class="btn btn-primary" name="button">Register</button>
+                <button type="button" class="btn btn-danger" name="button">Cancel</button>
+              </div>
+            </div>
+
           </div>
         </form>
       </div>
