d2314886bfe98973cda061f0ed9c274b3f899908
Yansiska <yansiska01@gmail.ocm>
2018-07-28 11:10:52 +0700

    Change Form registration

--- application/views/registration.php
+++ application/views/registration.php
@@ -12,7 +12,9 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
           <br>
           <br>
-          <h1 >FORM REGISTRATION</h1>
+          <h1 >Mohon mengisi detail pendaftaran acara di bawah ini</h1>
+          <br>
+          <br>
         </div>
 
         <form class="" style="width:100%" action="index.html" method="post">
@@ -20,24 +22,174 @@
 
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <div class="form-group">
-                <label for="">Category :</label>
+                <label for="">T-shirt Size / Ukuran T-shirt:</label>
+                  <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                  </select>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nama Depan / First Name :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nama Belakang / Surname :</label>
                 <input type="text" class="form-control" name="" value="">
               </div>
             </div>
 
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="form-group">
-                <label for="">Field1 :</label>
+                <label for="">Jenis Kelamin / Gender :</label>
+                  <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Laki-laki</option>
+                    <option>Perempuan</option>
+                  </select>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nomor Identitas (KTP / SIM / KITAS / Passport) / ID Number :</label>
                 <input type="text" class="form-control" name="" value="">
               </div>
             </div>
 
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="form-group">
-                <label for="">Field2 :</label>
+                <label for="">Tanggal Lahir / Date of Birth :</label>
+                <input type="Date" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Golongan Darah / Blood Type :</label>
+                <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>AB</option>
+                    <option>O</option>
+                  </select>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Alamat / Address :</label>
                 <input type="text" class="form-control" name="" value="">
               </div>
             </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Kota / City :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Provinsi / Province :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Negara / Country :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nomor Telepon / Phone Number :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Email / Email Address :</label>
+                <input type="email" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nama Kontak Darurat / Emergency Contact Name :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Nomor Kontak Darurat / Emergency Contact Number :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Hubungan / Relationship :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Are you MAP Club member [Yes or No] :</label>
+                <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Do you have any disease/ special condition/ allergic? [Yes or No] :</label>
+                <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Do you wear NB running(not lifestyle) shoes [Yes or No] :</label>
+                <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Do you wear NB running (not lifestyle) apparel [Yes or No] :</label>
+                <select class="form-control" name="" value="">
+                    <option>Choose a selection</option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="form-group">
+                <label for="">Name of Running Group (Fill "N/A" if don't have any) :</label>
+                <input type="text" class="form-control" name="" value="">
+              </div>
+            </div>
+
+            
 
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="form-group">
